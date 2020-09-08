8bb005facf0c866fe29b124f22039e83352cb50b
Adam <adammonast@Adams-MacBook-Pro.local>
2020-09-08 16:52:41 -0400

    initial functions review commit

--- 06-Functions/01-Function-Declaration-Vs-Expressions.js
+++ 06-Functions/01-Function-Declaration-Vs-Expressions.js
@@ -1,1 +1,15 @@
-// I hate git
+// Function Declaration
+function walk() {
+  console.log("walk");
+}
+
+// Function Expression
+// Anonymous function (function without a name)
+// Assigning a function to a variable makes the variable contain a refernce to the function
+const run = function () {
+  console.log("run");
+};
+// Can call the reference like a function
+run();
+let move = run;
+move();
